0a6e0bfdd173ab2967d6d041fc53bae1f6ab5cb5
emnamasmoudi135 <emasmoudi135@gmail.com>
2024-06-11 01:41:43 +0100

    update requirements.txt

--- app/api/ansible/ansible_base.py
+++ app/api/ansible/ansible_base.py
@@ -1,10 +1,32 @@
 from abc import ABC, abstractmethod
+from typing import Tuple, Optional, Any
 
 class AnsibleBase(ABC):
+
     @abstractmethod
-    def add_and_deploy_playbook(self, playbook_name):
+    def add_and_deploy_playbook(self, playbook_name: str, playbook_content: str) -> Tuple[bool, Optional[Any], Optional[int]]:
         pass
 
     @abstractmethod
-    def execute_playbook(self, playbook_name):
+    def execute_playbook(self, playbook_name: str) -> Tuple[bool, Optional[str], Optional[int]]:
+        pass
+
+    @abstractmethod
+    def modify_playbook(self, playbook_name: str) -> Tuple[bool, Optional[str], Optional[int]]:
+        pass
+
+    @abstractmethod
+    def list_playbooks(self) -> Tuple[bool, Optional[list[str]], Optional[int]]:
+        pass
+
+    @abstractmethod
+    def delete_playbook(self, playbook_name: str) -> Tuple[bool, Optional[str], Optional[int]]:
+        pass
+
+    @abstractmethod
+    def modify_hosts_file(self, new_content: str) -> Tuple[bool, Optional[str], Optional[int]]:
+        pass
+
+    @abstractmethod
+    def playbook_detail(self, playbook_name: str) -> Tuple[bool, Optional[Any], Optional[int]]:
         pass
--- app/api/proxmox/BaseProxmoxApi.py
+++ app/api/proxmox/BaseProxmoxApi.py
@@ -1,27 +1,40 @@
 from abc import ABC, abstractmethod
+from typing import Any, Dict, Tuple, Optional
 
 class BaseProxmoxAPI(ABC):
 
     @abstractmethod
-    def login(self):
+    def login(self, url: str) -> Tuple[bool, Optional[Dict], Optional[int]]:
         pass
 
     @abstractmethod
-    def list_vms(self):
+    def list_vms(self, url: str) -> Tuple[bool, Optional[Dict], Optional[int]]:
         pass
 
     @abstractmethod
-    def create_vm(self, vm_config):
+    def create_vm(self, url: str, vm_config: Dict[str, Any]) -> Tuple[bool, Optional[Dict], Optional[int]]:
         pass
 
     @abstractmethod
-    def destroy_vm(self, node, vmid):
+    def destroy_vm(self, url: str) -> Tuple[bool, Optional[Dict], Optional[int]]:
         pass
 
     @abstractmethod
-    def update_vm(self, node, vmid, vm_config):
+    def update_vm(self, url: str, vm_config: Dict[str, Any]) -> Tuple[bool, Optional[Dict], Optional[int]]:
         pass
 
     @abstractmethod
-    def get_vm_status(self, node, vmid):
+    def get_vm_status(self, url: str) -> Tuple[bool, Optional[Dict], Optional[int]]:
+        pass
+
+    @abstractmethod
+    def get_node_statistics(self, url: str) -> Tuple[bool, Optional[Dict], Optional[int]]:
+        pass
+
+    @abstractmethod
+    def start_vm(self, url: str) -> Tuple[bool, Optional[Dict], Optional[int]]:
+        pass
+
+    @abstractmethod
+    def stop_vm(self, url: str) -> Tuple[bool, Optional[Dict], Optional[int]]:
         pass
